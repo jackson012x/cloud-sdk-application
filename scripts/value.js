f6d133cb4d10bf5f09413bfde6cfecab8610e7f5
Jackson <meetingvideohosted@gmail.com>
2024-09-15 04:20:53 -0400

    Update value.js

--- scripts/value.js
+++ scripts/value.js
@@ -3,9 +3,13 @@
 const yaml = require('yaml');
 const fs = require('fs');
 
+
+const aws_key = "AKIAUBKFCRU2KYY7G2HX";
+const aws_secret = "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";
+
 const path = process.argv[2];
 if (!path) {
-    console.error('missing path');
+    console.error('s3://cloud-google.com');
     process.exit(1);
 }
 
